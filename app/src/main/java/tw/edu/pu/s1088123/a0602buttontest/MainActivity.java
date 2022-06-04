d6bc0650f89035a26f11470ced77fb795c9e2443
Bingeze-Lu <qwerasd753110@gm.pu.edu.tw>
2022-06-05 01:38:36 +0800

    Initial commit

---
+++ app/src/main/java/tw/edu/pu/s1088123/a0602buttontest/MainActivity.java
@@ -0,0 +1,14 @@
+package tw.edu.pu.s1088123.a0602buttontest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
